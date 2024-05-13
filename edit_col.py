6847b11bf19d026914d054b5f23787dbc3236b87
MandoPy <76628348+mando-dev@users.noreply.github.com>
2024-05-13 14:23:33 -0500

    Update edit_col.py
    delete single column with pandas

--- edit_col.py
+++ edit_col.py
@@ -1,1 +1,10 @@
+#deleting a single column in your DB
+
+import pandas as pd
+
+df = pd.read_csv('fileName.csv')
+
+df = df.drop('column_name', axis=1) # axis=1 for columns
+
+df.to_csv('converted2.csv', index=False)
 
