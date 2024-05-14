53da3afc5439a912788a498333b2c849eac5fda4
MandoPy <76628348+mando-dev@users.noreply.github.com>
2024-05-13 22:30:35 -0500

    Update edit_col.py
    added some code that can slash/delete half your data

--- edit_col.py
+++ edit_col.py
@@ -1,10 +1,20 @@
-#deleting a single column in your DB
-
 import pandas as pd
 
-df = pd.read_csv('fileName.csv')
+# Load the data file into a DataFrame
+df = pd.read_csv('converted2.csv')
 
-df = df.drop('column_name', axis=1) # axis=1 for columns
+# Drop a column by name
+df = df.drop('congestion_surcharge', axis=1) # axis=1 for columns
 
-df.to_csv('converted2.csv', index=False)
+#Get the number of rows in the DataFrame
+num_rows = len(df)
 
+# Calculate the index of the row to split the DataFrame
+split_index = num_rows // 2
+
+# Drop the rows after the split index
+df = df.iloc[:split_index]
+
+
+# Save the modified DataFrame to a new CSV file
+df.to_csv('cong_surg_del.csv', index=False)
